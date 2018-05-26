3944586460576f96bfbe83a87d6281be2a974958
Matias-Espinoza <mespinoza@ubicomp-labs.com>
2018-05-25 23:08:00 -0400

    Se agregaron efectos al hacer scroll y direcciones con scroll dentro la pagina

---
+++ assets/js/scroll.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  $('a[href^="#a"]').click(function() {
+    var destino = $(this.hash);
+    if (destino.length == 0) {
+      destino = $('a[name="' + this.hash.substr(1) + '"]');
+    }
+    if (destino.length == 0) {
+      destino = $('html');
+    }
+    $('html, body').animate({ scrollTop: destino.offset().top }, 1000);
+    return false;
+  });
+});
